84ff6963a4c74a0e38220318ec8a2b8cfa3fe299
Oluwajuwon165 <Oladitioluwajuwon165@gmail.com>
2022-10-11 12:50:42 -0700

    Fix

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python3
 
 """Define classes for a singly-linked list."""
@@ -33,18 +34,7 @@ class Node:
 
     @next_node.setter
     def next_node(self, value):
-        if not isinstance(value, Node) and value imp = tmp.next_node
-            new.next_node = tmp.next_node
-            tmp.next_node = new
-
-    def __str__(self):
-        """Define the print() representation of a SinglyLinkedList."""
-        values = []
-        tmp = self.__head
-        while tmp is not None:
-            values.append(str(tmp.data))
-            tmp = tmp.next_node
-        return ('\n'.join(values))s not None:
+        if not isinstance(value, Node) and value is not None:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
@@ -74,4 +64,15 @@ class SinglyLinkedList:
             tmp = self.__head
             while (tmp.next_node is not None and
                     tmp.next_node.data < value):
-                t
+                tmp = tmp.next_node
+            new.next_node = tmp.next_node
+            tmp.next_node = new
+
+    def __str__(self):
+        """Define the print() representation of a SinglyLinkedList."""
+        values = []
+        tmp = self.__head
+        while tmp is not None:
+            values.append(str(tmp.data))
+            tmp = tmp.next_node
+        return ('\n'.join(values))
