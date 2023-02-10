6f478b43f11c54a6603a657a2e0dd4e3779d32a6
Oluwajuwon165 <Oladitioluwajuwon165@gmail.com>
2023-02-10 01:59:46 +0100

    JS

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,13 +1,11 @@
-#!/usr/bin/nod
+#!/usr/bin/node
 
 const fs = require('fs');
+const filename = process.argv[2];
+const content = process.argv[3];
 
-const filePath = process.argv[2];
-const data = process.argv[3];
-
-fs.writeFile(filePath, data, 'utf-8', (err) => {
-  if (err) {
-    console.error(err);
-    return;
+fs.writeFile(filename, content, 'utf-8', (error) => {
+  if (error) {
+    console.log(error);
   }
 });
