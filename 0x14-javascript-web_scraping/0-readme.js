a9988914e49e2639e5180f0af21394687710c00a
Oluwajuwon165 <Oladitioluwajuwon165@gmail.com>
2023-02-10 01:27:26 +0100

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,16 +1,14 @@
 #!/usr/bin/node
 
-import sys
+const fs = require('fs');
 
-def read_file(file_path):
-    try:
-        with open(file_path, 'r', encoding='utf-8') as file:
-            content = file.read()
-            print(content)
-    except Exception as e:
-        print("An error occurred:", e)
+const filePath = process.argv[2];
 
-if _name_ == "_main_":
-    file_path = sys.argv[1]
-    read_file(file_path)
-#I am done
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  console.log(data);
+});
